d97b0263f132224be997779d906b4067e37759a5
Adryannunes <adryanhenrique62@gmail.com>
2025-02-10 16:01:28 -0300

    feat: config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,17 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        "neon-green": "#A7FF84", 
+      },
+      fontFamily: {
+        montserrat: ["Montserrat", "sans-serif"],
+        bebas: ["Bebas Neue", "sans-serif"],
+        oswald: ["Oswald", "sans-serif"],
+        anton: ["Anton", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
