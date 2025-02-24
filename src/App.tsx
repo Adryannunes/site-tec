49fe5a9b877501735cfd4b3fc72523dbe3803cc2
Adryannunes <adryanhenrique62@gmail.com>
2025-02-24 09:02:05 -0300

    feaat: reacr router

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,37 @@
+// src/App.tsx
 import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Navbar } from "./components/Navbar";
 import { Footer } from "./components/Footer";
-import { Patrocinadores } from "./components/Patrocinadores";
+import { Inicio } from "./components/inicio";
+import { Elenco } from "./components/Elenco";
+import { Ribeirao } from "./components/Ribeirao";
+import Transparencia from "./pages/Transparencia";
 
-const App: React.FC = () => {
+const Home: React.FC = () => {
   return (
     <div className="flex flex-col min-h-screen">
       <Navbar />
-      <Patrocinadores />
-      {/* Conteúdo da página */}
+      <Inicio />
+      {/* Conteúdo da página principal */}
       <div className="flex-grow">
-        {/* Aqui vai o conteúdo principal */}
+        {/* Outros componentes ou conteúdo */}
       </div>
-
+      <Elenco />
+      <Ribeirao />
       <Footer />
     </div>
+  );
+};
+
+const App: React.FC = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/transparencia" element={<Transparencia />} />
+      </Routes>
+    </BrowserRouter>
   );
 };
 
